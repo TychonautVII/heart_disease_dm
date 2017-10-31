ed8c6069db59e71f7d2c352db7cab0c9b3b210cf
Peter Mariani <peter@Peters-MacBook-Pro.local>
2017-10-31 11:06:53 -0400

    functionalized the preprocessing code

---
+++ heart_disease/globals.py
@@ -0,0 +1,5 @@
+code_path_str = '/Users/peter/OneDrive/Peter/JHU/DataMining/project/heart_disease/heart_disease/'
+
+data_path_str = '/Users/peter/OneDrive/Peter/JHU/DataMining/project/data/'
+
+output_path_str = '/Users/peter/OneDrive/Peter/JHU/DataMining/project/output/'
---
+++ heart_disease/preprocessing.py
@@ -0,0 +1,36 @@
+import numpy as np
+from heart_disease.globals import code_path_str, data_path_str
+import pandas as pd
+
+
+def load_raw_data(data_file_name_str):
+    """This function loads files located in the global data_path_str directory
+    matching the format of the processed.x.data files from the UCI repository.
+    It returns a pandas data frame"""
+
+    # Get Metadata
+    meta_data_df = pd.read_csv(code_path_str+'meta_data.csv')
+    meta2pdtype = {'numeric':np.float64,'bool':np.bool,'categorical':'category'}
+
+
+    # Load in the actual data
+    column_names = ['age', 'sex', 'cp', 'trestbps',
+                    'chol', 'fbs', 'restecg', 'thalach',
+                    'exang', 'oldpeak', 'slope',
+                    'ca', 'thal', 'num']
+
+    # Associate Columns with datatypes
+    dtype_map = {}
+    for col in column_names:
+        meta_datatype = meta_data_df['datatype'].loc[meta_data_df['name'] == col].iloc[0]
+        dtype_map[col] = meta2pdtype[meta_datatype]
+
+    # Read in the data
+    data_df = pd.read_csv((data_path_str + data_file_name_str),
+                          names=column_names, dtype=dtype_map)
+
+    # Basic Pre-processing:
+    data_df['ispos_truth'] = data_df['num'] != '0'
+    data_df.drop('num', 1, inplace=True)
+
+    return data_df
