a689f72d8185832508f768058a8c6548b208101d
Peter Mariani <peter@Peters-MacBook-Pro.local>
2017-11-10 14:32:44 -0500

    fixed the logger, and adjusted to follow convention

---
+++ heart_disease/main.py
@@ -0,0 +1,32 @@
+import numpy as np
+import logging
+import pandas as pd
+from copy import deepcopy as cp
+
+from heart_disease.globals import code_path_str, data_path_str
+import heart_disease.preprocessing as pre_proc
+
+
+# Create Loger Object
+LOG_FMT = '%(asctime)s|%(name)s|%(levelname)s|%(message)s'
+logging.basicConfig(level=logging.DEBUG, format=LOG_FMT)
+logger = logging.getLogger(__name__)
+
+# import logging
+# logger = logging.getLogger('heart_disease')
+#
+# sh = logging.StreamHandler()
+# formatter = logging.Formatter(LOG_FMT)
+# sh.setFormatter(formatter)
+# logger.addHandler(sh)
+
+
+# Get Metadata
+meta_data_df = pd.read_csv(code_path_str+'meta_data.csv')
+
+data_file_name_str = 'processed.va.data.txt'
+
+data_df = pre_proc.load_raw_data(data_file_name_str)
+
+clean_df, preproc_dict = pre_proc.clean_data(data_df)
+clean_df.head()
--- heart_disease/preprocessing.py
+++ heart_disease/preprocessing.py
@@ -3,7 +3,7 @@ from heart_disease.globals import code_path_str, data_path_str, outcome_to_learn
 from sklearn.preprocessing import Imputer, StandardScaler
 import pandas as pd
 import logging
-
+logger = logging.getLogger(__name__)
 
 def load_raw_data(data_file_name_str):
     """This function loads files located in the global data_path_str directory
@@ -42,24 +42,24 @@ def __clean_numeric(data_df, preprocessing_objc):
 
     # Impute Missing Values
     if 'numeric_imputer' not in preprocessing_objc:
-        logging.info("Constructing New Numeric Imputer")
+        logger.info("Constructing New Numeric Imputer")
         numeric_imputer = Imputer(missing_values=np.nan, strategy='mean', axis=0, verbose=0, copy=True)
         numeric_imputer.fit(data_df)
         preprocessing_objc['numeric_imputer'] = numeric_imputer
     else:
-        logging.info("Using Precomputed Numeric Imputer")
+        logger.info("Using Precomputed Numeric Imputer")
         numeric_imputer = preprocessing_objc['numeric_imputer']
 
     imputed_df = pd.DataFrame(numeric_imputer.transform(data_df), columns=data_df.columns)
 
     # Scale values to zero mean and unit variance
     if 'numeric_scaler' not in preprocessing_objc:
-        logging.info("Constructing New Numeric Scalar")
+        logger.info("Constructing New Numeric Scalar")
         numeric_scaler = StandardScaler()
         numeric_scaler.fit(imputed_df)
         preprocessing_objc['numeric_scaler'] = numeric_scaler
     else:
-        logging.info("Using Precomputed Numeric Scalar")
+        logger.info("Using Precomputed Numeric Scalar")
         numeric_scaler = preprocessing_objc['numeric_scaler']
 
 
@@ -70,12 +70,12 @@ def __clean_numeric(data_df, preprocessing_objc):
 def __clean_bool(data_df, preprocessing_objc):
 
     if 'bool_imputer' not in preprocessing_objc:
-        logging.info("Constructing New Boolean Imputer")
+        logger.info("Constructing New Boolean Imputer")
         bool_imputer = Imputer(missing_values=np.nan, strategy='most_frequent', axis=0, verbose=0, copy=True)
         bool_imputer.fit(data_df)
         preprocessing_objc['bool_imputer'] = bool_imputer
     else:
-        logging.info("Using Precomputed Boolean Imputer")
+        logger.info("Using Precomputed Boolean Imputer")
         bool_imputer = preprocessing_objc['bool_imputer']
 
     imputed_df = pd.DataFrame(bool_imputer.transform(data_df),columns=data_df.columns)
@@ -86,26 +86,26 @@ def __clean_categorical(data_df, preprocessing_objc):
 
     # Impute Missing Values
     if 'cat_imputer' not in preprocessing_objc:
-        logging.info("Constructing New Categorical Imputer")
+        logger.info("Constructing New Categorical Imputer")
         cat_imputer = Imputer(missing_values=np.nan, strategy='most_frequent', axis=0, verbose=0, copy=True)
         cat_imputer.fit(data_df)
         preprocessing_objc['cat_imputer'] = cat_imputer
     else:
-        logging.info("Using Precomputed Categorical Imputer")
+        logger.info("Using Precomputed Categorical Imputer")
         cat_imputer = preprocessing_objc['cat_imputer']
 
     imputed_df = pd.DataFrame(cat_imputer.transform(data_df), columns=data_df.columns, dtype=int)
 
     # One Hot Encode Categorical Features as Binary Features
     if 'one_hot_dict' not in preprocessing_objc:
-        logging.info("Constructing New One Hot Dictionary")
+        logger.info("Constructing New One Hot Dictionary")
         categories = list(imputed_df.columns)
         one_hot_dict = {}
         for cat in categories:
             one_hot_dict[cat] = np.unique(imputed_df[cat])
         preprocessing_objc['one_hot_dict'] = one_hot_dict
     else:
-        logging.info("Using precomputed One Hot Dictionary")
+        logger.info("Using precomputed One Hot Dictionary")
         one_hot_dict = preprocessing_objc['one_hot_dict']
         # TODO: optionally Raise error / warning when value not in dictionary is in this dataframe
 
